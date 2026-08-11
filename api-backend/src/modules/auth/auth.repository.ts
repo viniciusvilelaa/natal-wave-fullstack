a713469880855729d8b6f9f46a17bb405c766b16
viniciusvilelaa <ninobez@icloud.com>
2026-08-11 16:29:23 -0300

    New(auth.repository.ts): Implementado codigo authRepository que exporta funcoes com queries prisma a fim de ser utilizado no auth.service

---
+++ api-backend/src/modules/auth/auth.repository.ts
@@ -0,0 +1,59 @@
+import { prisma } from "../../lib/prisma"
+
+export const authRepository = {
+
+    //Encontra usuario especifico atraves do email
+    findUserByEmail(email: string) {
+        return prisma.user.findUnique({ where: { email } });
+    },
+    
+    //Cria usuario
+    createUser(data: { name: string, email: string, passwordHash: string }) {
+        return prisma.user.create({ data });
+    },
+
+    //Encontra usuario pelo ID
+    findUserById(id: string) {
+        return prisma.user.findUnique({
+            where: { id },
+            select: { id: true, name: true, email: true, createdAt: true }
+        });
+    },
+
+    //Cria um novo refreshToken
+    createRefreshToken(data: {
+        userId: string;
+        tokenHash: string;
+        expiresAt: Date;
+    }) {
+        return prisma.refreshToken.create({ data });
+    },
+
+    //Encontra o token pelo hash
+    findRefreshTokenByHash(tokenHash: string) {
+        return prisma.refreshToken.findUnique({
+            where: { tokenHash },
+            include: { user: true }
+        })
+    },
+
+    //Revoga o refreshToken
+    revokeRefreshToken(id: string, replacedByToken?: string) {
+        return prisma.refreshToken.update({
+            where: { id },
+            data: {
+                revokedAt: new Date(),
+                ...(replacedByToken && { replacedByToken }),
+            },
+        });
+    },
+
+    //Revoga todos os refreshTokens do usuario
+    revokeAllUserRefreshTokens(userId: string) {
+        return prisma.refreshToken.updateMany({
+            where: { userId, revokedAt: null },
+            data: { revokedAt: new Date() },
+        });
+    },
+
+}
