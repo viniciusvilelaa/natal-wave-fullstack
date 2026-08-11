99e00f8dc9e4268eb6cf1ece004e5fe972036130
viniciusvilelaa <ninobez@icloud.com>
2026-08-11 10:13:03 -0300

    New(async-handler): Implementada funcao para tratar erros das funcoes assincronas

---
+++ api-backend/src/utils/async-handler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+type AsyncRouteHandler = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => Promise<unknown>;
+
+export function asyncHandler(handler: AsyncRouteHandler) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        handler(req, res, next).catch(next);
+    };
+}
