81d2bda822915682b613cc87f3016dac2e90c52b
viniciusvilelaa <ninobez@icloud.com>
2026-08-17 09:48:28 -0300

    Feat(auth.routes): Implementado arquivo de rotas de auth

---
+++ api-backend/src/modules/auth/auth.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import * as authController from "./auth.controller";
+import { validate } from "../../middleware/schema.middleware"
+import { authMiddleware } from "../../middleware/auth.middleware";
+import { asyncHandler } from "../../utils/async-handler";
+import { registerSchema, loginSchema, refreshTokenSchema } from "./auth.validation";
+
+const router = Router();
+
+router.post(
+    "/register",
+    validate(registerSchema),
+    asyncHandler(authController.register)
+);
+
+router.post(
+    "/login",
+    validate(loginSchema),
+    asyncHandler(authController.login)
+);
+
+router.post(
+    "/refresh",
+    validate(refreshTokenSchema),
+    asyncHandler(authController.refresh)
+);
+
+router.post(
+    "/logout",
+    validate(refreshTokenSchema),
+    asyncHandler(authController.logout)
+);
+
+router.get(
+    "/me",
+    authMiddleware,
+    asyncHandler(authController.me)
+);
+
+export default router;
