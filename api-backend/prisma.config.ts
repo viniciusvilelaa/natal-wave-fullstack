e44b8d21e49da57127054d7d3771bf030bfbe988
viniciusvilelaa <ninobez@icloud.com>
2026-08-19 15:38:49 -0300

    Fix: PrismaCLI e Prisma Client atualizados para versao mais recente estavel

--- api-backend/prisma.config.ts
+++ api-backend/prisma.config.ts
@@ -2,9 +2,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL as string,
   },
 });
