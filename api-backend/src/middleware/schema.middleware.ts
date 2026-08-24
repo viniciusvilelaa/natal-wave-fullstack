5cf71e5018596e098d3a32bfadfd541adbf819c4
viniciusvilelaa <ninobez@icloud.com>
2026-08-24 15:57:37 -0300

    New(schema.middleware): Implementado o validate query para validar dados que chegam atraves da query da requisicao

--- api-backend/src/middleware/schema.middleware.ts
+++ api-backend/src/middleware/schema.middleware.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodSchema } from "zod";
+import { Schema, ZodSchema } from "zod";
 
 export function validate(schema: ZodSchema) {
   return (req: Request, _res: Response, next: NextFunction) => {
     req.body = schema.parse(req.body);
     next();
   };
+}
+
+export function validateQuery(schema: ZodSchema) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    req.query = schema.parse(req.query) as any;
+    next()
+  }
 }
