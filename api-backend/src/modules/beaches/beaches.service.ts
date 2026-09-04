6ec6fc6c9f12020f7c17021477a5200f1adb3b5f
viniciusvilelaa <ninobez@icloud.com>
2026-09-04 11:55:55 -0300

    New(beaches.service): Implementada funcao createBeach no service

--- api-backend/src/modules/beaches/beaches.service.ts
+++ api-backend/src/modules/beaches/beaches.service.ts
@@ -4,14 +4,23 @@ import { BeachIdParamInput, CreateBeachInput, SearchBeachQueryInput } from "./be
 
 
 export async function createBeach(data: CreateBeachInput){
-    
+    const beachExists = await beachesRepository.findByNameAndCity(data.name, data.city);
+
+    if(beachExists){
+        throw new ApiError(409, "Beach already exists in this city");
+    }
+
+    const createdBeach = await beachesRepository.createBeach(data);
+
+    return createdBeach
+
 }
 
 
-export async function findAll(filters: SearchBeachQueryInput) {
+export async function findMany(filters: SearchBeachQueryInput) {
 
 
-    const [beaches, total] = await beachesRepository.findAll(filters);
+    const [beaches, total] = await beachesRepository.findMany(filters);
 
     if (beaches.length < 1) {
         throw new ApiError(200, "No beaches are found");
