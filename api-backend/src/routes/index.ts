81b04244eb640ac43fd637fb818ef628d8d830af
viniciusvilelaa <ninobez@icloud.com>
2026-09-23 18:42:26 -0300

    Add(index.ts): Adicionado o router de beaches no arquivo de rotas geral

--- api-backend/src/routes/index.ts
+++ api-backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "../modules/auth/auth.routes";
+import beachesRouter from "../modules/beaches/beaches.routes";
 
 const router = Router();
 
 
-router.use('/auth', authRouter)
+router.use('/auth', authRouter);
+router.use('/beaches', beachesRouter);
 
 
 export default router
